83bc7b99ff75ccfbe7daa5b27c098f0be21c187a
aZong <azong@pop-os.localdomain>
2023-12-16 12:41:59 +0700

    Updated diagnostic sign

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -61,33 +61,28 @@ kana.setup({
 -- Set colorscheme
 vim.cmd("colorscheme kanagawa-wave") -- Wave | Lotus | Dragon
 
--- For diagnostic
+-- For diagnostics
+local signs = {
+	icons = {
+		[vim.diagnostic.severity.ERROR] = "",
+		[vim.diagnostic.severity.WARN] = "",
+		[vim.diagnostic.severity.INFO] = "",
+		[vim.diagnostic.severity.HINT] = "",
+	},
+	highlight = {
+		[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+		[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+		[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+		[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+	},
+}
+
 vim.diagnostic.config({
 	signs = {
-		text = {
-			[vim.diagnostic.severity.ERROR] = "",
-			[vim.diagnostic.severity.WARN] = "",
-			[vim.diagnostic.severity.HINT] = "",
-			[vim.diagnostic.severity.INFO] = "",
-		},
-		texthl = {
-			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-		},
-		numhl = {
-			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-		},
-		linehl = {
-			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-		},
+		text = signs.icons,
+		texthl = signs.highlight,
+		numhl = signs.highlight,
+		linehl = signs.highlight,
 	},
 	virtual_text = {
 		prefix = "󰄛 ", -- 󰄛 | ● | 󰝤
